e50a9294831cf55edbf32108b9121ac75b315db3
yeongjong310 <tohero21@naver.com>
2022-01-06 16:55:54 +0900

    feat: landing 디자인 일부 수정

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -7,14 +7,14 @@ import { stringToDOMArray } from '@/utils';
 import { serviceData } from './data';
 import mainImg from './assets/img/main.jpg';
 import styles from './Landing.module.scss';
-import { icon } from '@fortawesome/fontawesome-svg-core';
-import { faPhoneAlt } from '@fortawesome/free-solid-svg-icons';
+import { icon as _icon } from '@fortawesome/fontawesome-svg-core';
+import { faPhoneAlt, faPaperPlane, faHeadset } from '@fortawesome/free-solid-svg-icons';
 import { IconLookup } from '@fortawesome/fontawesome-common-types';
 
 /** @jsx transJSXtoDOM */
 export default class Landing extends Node {
-  getIconDOM() {
-    const iconTemplete = icon(faPhoneAlt as IconLookup, {}).html;
+  getIconDOM(icon: IconLookup) {
+    const iconTemplete = _icon(icon, {}).html;
     const $icon = stringToDOMArray(iconTemplete[0])[0] as HTMLElement;
     return $icon;
   }
@@ -22,7 +22,12 @@ export default class Landing extends Node {
   template() {
     return (
       <main class={styles.land}>
-        <img src={mainImg} class={styles.main__img} />
+        <div
+          style={{ background: `url(${mainImg})`, 'background-position': 'top', 'background-size': 'cover' }}
+          role="img"
+          aria-label="한강건설 회사 소개 이미지"
+          class={styles.main__img}
+        />
         <Title
           title="한강설비"
           description="신갈에서 제일 오래된 설비이면 과장되지 않는 가격 확실한 수리, 유지보수를 보장합니다. 수많은 노하우로
@@ -40,11 +45,11 @@ export default class Landing extends Node {
         <section class={styles.land__contact}>
           <h3 class={styles.landing__title}>고객 응대</h3>
           <div class={styles.contact__description}>
-            <p class={styles.contact__call}>고객센터: 031-000-0000</p>
-            <p class={styles.contact__call}>대표번호: 010-0000-0000</p>
-            <p class={styles.contact__email}>이메일: par10915@naver.com</p>
-            <a href={`tel:${process.env.callNumber}`}>
-              {this.getIconDOM.call(this)}
+            <p class={styles.contact__call}>{this.getIconDOM.call(this, faHeadset)} 031-000-0000</p>
+            <p class={styles.contact__call}>{this.getIconDOM.call(this, faPhoneAlt)} 010-0000-0000</p>
+            <p class={styles.contact__email}>{this.getIconDOM.call(this, faPaperPlane)} par10915@naver.com</p>
+            <a class={styles.contact__btn} href={`tel:${process.env.callNumber}`}>
+              {this.getIconDOM.call(this, faPhoneAlt)}
               <span>전화상담</span>
             </a>
           </div>
